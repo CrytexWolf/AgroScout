83424d344aed30f0ef3b2f66edf1c0f17c48503b
CrytexWolf <71540922+CrytexWolf@users.noreply.github.com>
2024-11-04 18:15:48 +0500

    deleted:    public/favicon.ico 	modified:   src/App.vue 	new file:   src/assets/LogoAgroScout.jpg 	renamed:    public/LogoAgroScout.png -> src/assets/LogoAgroScout.png 	new file:   src/assets/carbon_close-outline.svg 	new file:   src/assets/iconBasket.svg 	new file:   src/assets/iconMenu.svg 	new file:   src/assets/iconPhone.svg 	new file:   src/assets/iconTikTok.svg 	new file:   src/assets/iconVk.svg 	new file:   src/assets/iconinstagram.svg 	new file:   "src/assets/\320\277\320\265\321\200\320\262\321\213\320\271 \321\215\320\272\321\200\320\260\320\275.png" 	renamed:    src/components/Footer/Footer.vue -> src/components/Footer.vue 	new file:   src/components/Header.vue 	deleted:    src/components/Header/Header.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,51 @@
-<script setup>
+<script>
+import Header from '@/components/Header.vue'
 
+export default {
+  components: {
+    Header,
+  }
+};
 </script>
 
 <template>
-  <div>  TEST123 </div>
+  <Header />
 </template>
 
 <style>
-  * {
-    margin: 0;
-    padding: 0;
+  body {
+	margin: 0;
+	font-family: "Raleway", sans-serif;
+	font-size: 14px;
+	color: #6c7279;
+	-webkit-font-smoothing: antialiased;
+
+	background-color: #fff;
+}
+
+  *,
+  *:before,
+  *:after {
     box-sizing: border-box;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin: 0;
+  }
+
+  p {
+    margin: 0 0 10px;
+  }
+
+  .container {
+    max-width: 1300px;
+    width: 100%;
+    margin: 0 auto;
+    padding: 0 15px;
   }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<script>
+export default {};
+</script>
+
+<template>
+    <header class="header">
+        <div class="container">
+            <div class="header__inner">
+                <div class="header__inner--left">
+                    <div class="header__logo">
+                    <img src="../assets/LogoAgroScout.png" class="header__img">
+                    </div>
+                    <div class="haeder__number"><img src="../assets/iconPhone.svg" alt=""><a href="tel:+78612179118" type="phone">+7861 21791 18</a></div>
+                </div>
+                <div class="header__inner--right">
+
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+    .header {
+        height: 72px;
+        background-color: #151515;
+    }
+
+    .header__inner {
+        display: flex;
+        align-items: center;
+    }
+
+    .header__inner--left {
+        display: flex;
+    }
+
+    .haeder__number {
+        display: flex;
+        align-items: center;
+        margin-left: 11px;
+    }
+</style>
